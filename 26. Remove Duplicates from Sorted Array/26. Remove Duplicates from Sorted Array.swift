9a3d4e8a8a5ff0d11bb51fc386ff6eb40ea15c7c
Pawel Kowalczuk <riamf2@gmail.com>
2025-12-08 18:09:35 +0100

    Slow pointer solution

--- 26. Remove Duplicates from Sorted Array/26. Remove Duplicates from Sorted Array.swift
+++ 26. Remove Duplicates from Sorted Array/26. Remove Duplicates from Sorted Array.swift
@@ -1,4 +1,18 @@
 class Solution {
+
+    func removeDuplicatesSlowPointer() {
+        guard !nums.isEmpty else { return 0 }
+        var k = 1
+        for i in 1..<nums.count {
+            if nums[i] != nums[k-1] {
+                // unique
+                nums[k] = nums[i]
+                k += 1
+            }
+        }
+        return k
+    }
+    
     func removeDuplicates(_ nums: inout [Int]) -> Int {
         var i = 0
         var j = 1
