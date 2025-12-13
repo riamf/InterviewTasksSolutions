ed029ef7cdb3e9d8e4da939ce2a4abc0fc9f1020
Pawel Kowalczuk <riamf2@gmail.com>
2025-12-13 17:02:27 +0100

    Bad solution time limit exceded

--- 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.swift
+++ 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.swift
@@ -1,5 +1,13 @@
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
-        
+        var maxProfit = 0
+        for i in 0..<prices.count {
+            for j in (i+1)..<prices.count {
+                if prices[j] - prices[i] > maxProfit {
+                    maxProfit = prices[j] - prices[i]
+                }
+            }
+        }
+        return maxProfit
     }
 }
