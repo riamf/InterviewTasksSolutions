416042c35446bfebf577003c5b4ed037d8f893a1
Pawel Kowalczuk <riamf2@gmail.com>
2025-12-13 17:30:42 +0100

    Proper solution

--- 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.swift
+++ 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.swift
@@ -1,13 +1,18 @@
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
-        var maxProfit = 0
-        for i in 0..<prices.count {
-            for j in (i+1)..<prices.count {
-                if prices[j] - prices[i] > maxProfit {
-                    maxProfit = prices[j] - prices[i]
-                }
-            }
+        guard prices.count > 1 else {
+            return 0
         }
+        var maxProfit = 0
+        var minPrice = prices[0]
+        for i  in 1..<prices.count {
+            let profit = prices[i] - minPrice
+            if profit > maxProfit {
+                maxProfit = profit
+            }
+            minPrice = min(minPrice, prices[i])
+        }
+        
         return maxProfit
     }
 }
