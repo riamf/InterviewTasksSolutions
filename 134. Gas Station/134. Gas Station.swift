1b9b2a0ecdde6659de539f34a9858bd1ca6739e2
Pawel Kowalczuk <riamf2@gmail.com>
2026-01-10 14:28:00 +0100

    working solution but to slow without memoization

--- 134. Gas Station/134. Gas Station.swift
+++ 134. Gas Station/134. Gas Station.swift
@@ -7,23 +7,27 @@ class Solution {
         var counter = 0
         var steps = [Int]()
         var currentIndex = 0
+        var resultIndex = -1
         
         for i in (0..<gas.count) {
             print("Starting at \(i)")
-            var totalGas = gas[i]
+            var totalGas = 0
             var counter = 0
             var visited = Set<Int>()
+            var foundALoop = false
             var currentIndex = i
             while counter < 100, visited.count < gas.count {
+                totalGas += gas[currentIndex]
                 let firstPossibleIndexs = (0..<gas.count).filter { !visited.contains($0) }
                 if !firstPossibleIndexs.isEmpty {
                     let firstPossibleIndex = firstPossibleIndexs.first(where: { $0 > currentIndex }) ?? firstPossibleIndexs.first!
-                    print("Going to \(firstPossibleIndex) with totalGas \(totalGas)")
-                    let cost = cost[firstPossibleIndex]
+                    let cost = cost[currentIndex]
+                    print("Going to \(firstPossibleIndex) with totalGas \(totalGas) cost is \(cost)")
                     if totalGas >= cost {
                         totalGas -= cost
                         currentIndex = firstPossibleIndex
                         visited.insert(firstPossibleIndex)
+                        foundALoop = visited.count == gas.count
                     } else {
                         // this index is not possible
                         print("Cannot proceed to \(firstPossibleIndex) with totalGas \(totalGas) cost is \(cost)")
@@ -36,10 +40,14 @@ class Solution {
                 
                 counter += 1
             }
+            if foundALoop {
+                resultIndex = i
+                break
+            }
             
         }
         
-        return -1
+        return resultIndex
     }
 }
 
