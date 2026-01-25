605971d76aa249eb191115a633e100a1b3f9914a
Pawel Kowalczuk <riamf2@gmail.com>
2026-01-25 15:34:58 +0100

    not optimal but working

--- 42. Trapping Rain Water/42. Trapping Rain Water.swift
+++ 42. Trapping Rain Water/42. Trapping Rain Water.swift
@@ -1,7 +1,30 @@
+import UIKit
+
+
 class Solution {
-
-
-
+    func trap(_ height: [Int]) -> Int {
+        var waterTapped = 0
+        for (i, h) in height.enumerated() {
+            print("\(i): \(h)")
+            var leftMax = h
+            var rightMax = h
+            for j in (0..<i) {
+                if height[j] > leftMax {
+                    leftMax = height[j]
+                }
+            }
+            for j in (i+1..<height.count) {
+                if height[j] > rightMax {
+                    rightMax = height[j]
+                }
+            }
+            
+            waterTapped += min(leftMax, rightMax) - h
+        }
+        return waterTapped
+    }
 }
 
-let solution = Solution()
+let s = Solution()
+let height = [0,1,0,2,1,0,1,3,2,1,2,1]
+print(s.trap(height))
